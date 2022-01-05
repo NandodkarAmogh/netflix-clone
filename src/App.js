8f8815dfc069b9e3440398972477ccec02a0213d
Amogh Nandodkar <nandodkaramogh@gmail.com>
2022-01-05 09:47:20 +0530

    Signup and Login Screen build

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import './App.css';
-import HomeScreen from './components/HomeScreen'
+import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/LoginScreen';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
 
 function App() {
+  const user = null;
+
   return (
     <div className="app">
-      <HomeScreen />  
+      {/* <HomeScreen /> */}
       {/* 0b507d5c113cff5cde0e00499a8ae71a */}
+
+      <BrowserRouter>
+        
+          {!user ? (
+            <LoginScreen />
+            // <Route path="login" element ={<LoginScreen />} />
+          ) : (
+          <Routes>  
+            <Route 
+              exact path="/" 
+              element={<HomeScreen />} />
+          </Routes>
+          )}
+          
+          {/* <Route path="users" element={<Users />}>
+            <Route path="/" element={<UsersIndex />} />
+            <Route path=":id" element={<UserProfile />} />
+            <Route path="me" element={<OwnUserProfile />} /> */}
+          {/* </Route> */}
+        
+      </BrowserRouter>
     </div>
   );
 }
